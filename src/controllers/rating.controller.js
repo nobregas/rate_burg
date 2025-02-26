cb6caa936f4e2a34afbfb38cb0e00e0ffc53fcd2
Gabriel <gabrielnobregasantos2@gmail.com>
2025-02-26 09:42:00 -0300

    basic controllers structure

---
+++ src/controllers/rating.controller.js
@@ -0,0 +1,28 @@
+
+
+class RatingController {
+
+    getUserRatings = async (req, res) => {
+
+    }
+
+    getRestaurantRatings = async (req, res) => {
+
+    }
+
+    rateRestaurant = async (req, res) => {
+
+    }
+
+    deleteUserRating = async (req, res) => {
+        /**
+         * Only the user or an admin can delete a rating.
+         */
+
+
+    }
+
+
+}
+
+export default new RatingController()
---
+++ src/controllers/restaurant.controller.js
@@ -0,0 +1,26 @@
+
+
+class RestaurantController {
+
+    create = async (req, res) => {
+
+    }
+
+    getOne = async (req, res) => {
+
+    }
+
+    findAll = async (req, res) => {
+
+    }
+
+    update = async (req, res) => {
+
+    }
+
+    delete = async (req, res) => {
+
+    }
+}
+
+export default new RestaurantController()
