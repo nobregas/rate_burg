3f75b83d38ef260bfe5b8079f22f490462b59d48
nobrega <gabrielnobregasantos2@gmail.com>
2025-03-08 00:30:11 -0300

    fix: mongoose exceptions

--- src/middleware/errorhandler.js
+++ src/middleware/errorhandler.js
@@ -1,20 +1,28 @@
-import HttpException from "../exceptions/root.js"
+import HttpException from "../exceptions/root.js";
 import { httpStatus } from "../enums/httpstatus.js";
-import { InternalException } from "../exceptions/internalException.js"
+import { InternalException, ValidationException } from "../exceptions/internalException.js";
+import { ErrorCodes } from "../enums/errorcodes.js";
+import mongoose from 'mongoose';
 
 export const errorHandler = (method) => {
     return async (req, res, next) => {
         try {
-            await method(req, res, next)
+            await method(req, res, next);
         } catch (err) {
-            let exception  
-
-            if (err instanceof HttpException) {
-                exception = err
-            } else {
-                exception = new InternalException("Something went wrong", err, httpStatus.INTERNAL_SERVER_ERROR)
+            let exception;
+            
+            if (err instanceof mongoose.Error.ValidationError) {
+                exception = new ValidationException(err.message, err.errors, ErrorCodes.VALIDATION_ERROR);
+            } 
+            else if (err instanceof HttpException) {
+                exception = err;
+            } 
+            else {
+                console.error("Error unsolved:", err);
+                exception = new InternalException("Intern Exception", err, httpStatus.INTERNAL_SERVER_ERROR);
             }
-            next(exception)
+            
+            next(exception);
         }
-    }
-}
+    };
+};
