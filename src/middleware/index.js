6472a4f8473ee424d2f14328593735bc23d1366b
nobrega <gabrielnobregasantos2@gmail.com>
2025-03-08 00:39:19 -0300

    refactor: optimize middleware imports

---
+++ src/middleware/index.js
@@ -0,0 +1,5 @@
+export { default as authMiddleware } from './auth.middleware.js';
+export { default as errorHandler } from './errorhandler.middleware.js';
+export { default as errorMiddleWare } from './errors.middleware.js';
+export { default as rateLimiter } from './rateLimit.middleware.js';
+export { default as roleMiddleware } from './role.middleware.js';
